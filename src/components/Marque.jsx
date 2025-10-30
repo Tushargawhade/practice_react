322f6a4ca8d30ecefea5fe5bf3a791f3d54a638e
Tusshar <tushargawhade7@gmail.com>
2025-10-30 18:33:31 +0530

    created

---
+++ src/components/Marque.jsx
@@ -0,0 +1,34 @@
+import { motion } from "motion/react";
+import React from "react";
+
+function Marque({ imgurl, direction }) {
+  return (
+    <div className="flex w-full  overflow-hidden">
+      <motion.div
+        initial={{ x: direction === "left" ? "0" : "-100%" }}
+        animate={{ x: direction === "left" ? "-100%" : "0" }}
+        transition={{ ease: "linear", duration: 15, repeat: Infinity }}
+        className="flex flex-shrink-0 gap-40 py-10 pr-40"
+      >
+        {imgurl.map((url, index) => (
+          <img key={index} src={url} className="" />
+        ))}
+      </motion.div>
+
+      <motion.div
+        initial={{ x: direction === "left" ? "0" : "-100%" }}
+        animate={{ x: direction === "left" ? "-100%" : "0" }}
+        transition={{ ease: "linear", duration: 15, repeat: Infinity }}
+        className="flex flex-shrink-0 gap-40 py-10 pr-40"
+      >
+        {imgurl.map((url, index) => (
+          <img key={index} src={url} className="" />
+        ))}
+      </motion.div>
+
+
+    </div>
+  );
+}
+
+export default Marque;
